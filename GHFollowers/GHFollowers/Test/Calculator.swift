00170dace09a2408fe47f921289c30931c999eff
Aleksei <alekseyzubel@gmail.com>
2026-02-24 01:49:10 +0300

    add some tests

---
+++ GHFollowers/GHFollowers/Test/Calculator.swift
@@ -0,0 +1,14 @@
+//
+//  Calculator.swift
+//  GHFollowers
+//
+//  Created by Алексей Зубель on 24.02.26.
+//
+
+import Foundation
+
+struct Calculator {
+    func getSum(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
